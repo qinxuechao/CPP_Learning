13b0758ccbfddae5f53bfc46350db4d36b403d77
xqin <xqin@hantak.cn>
2022-03-21 13:54:00 +0800

    add my first cpp file

---
+++ ch2/myfisrt.cpp
@@ -0,0 +1,14 @@
+//
+// Created by xqin on 3/21/2022.
+//
+
+#include "iostream"
+
+int main() {
+    using namespace std;
+    count << "Come up and C++ me some time.";
+    count << endl;
+    count << "You won't regret it!" << endl;
+    return 0; // terminate main()
+}
+
