b276d7239566cacab3637ac2ede33a789ab21920
xqin <xqin@hantak.cn>
2022-03-24 11:34:14 +0800

    add instrument

---
+++ ch4/instr.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Xuechao Qin on 3/24/2022.
+//
+
+#include "iostream"
+
+int main() {
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name:\n";
+    cin >> name;
+    cout << "Enter your favorite dessert: \n";
+    cin >> dessert;
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+
+    return 0;
+}
