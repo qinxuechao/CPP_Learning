a1da1257d73fb14e9f9d5ea991df11818620db54
unknown <qinxuechao@yahoo.com>
2022-03-29 00:38:04 +0800

    Add pointers

---
+++ ch4/ptrstr.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Xuechao Qin on 3/28/2022.
+//
+
+#include "iostream"
+#include "cstring"
+
+int main() {
+    using namespace std;
+    char animal[20] = "bear";
+    const char *bird = "wren";
+    char *ps;
+
+    cout << animal << " and ";
+    cout << bird << "\n";
+    //cout << ps << "\n",  //may display garbage, may cause a cream
+
+    cout << "Enter a kind of animal:";
+    cin >> animal;
+    // cin >> ps; Too horible a blunder to try; ps doesn't point to allocated space
+
+    ps = animal;
+    cout << ps << "!\n";
+    cout << "Before using strcpy():\n";
+    cout << animal << " at " << (int *) ps << endl;
+
+    ps = new char[strlen(animal) + 1];  // get   new storage
+    strcpy(ps, animal);
+//    strcpy_s(ps, sizeof(animal) + 1, animal);
+    cout << ps << " at " << (int *) ps << endl;
+    delete ps;
+
+    return 0;
+}
