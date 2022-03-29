49acb5ff1f0222369243c40b076e2f7121165ddf
xqin <xqin@hantak.cn>
2022-03-29 12:43:23 +0800

    add ch5

---
+++ ch5/plus_one.cpp
@@ -0,0 +1,16 @@
+//
+// Created by xcqin on 3/29/2022.
+//
+
+#include "iostream"
+
+int main() {
+    using std::cout;
+    int a = 20;
+    int b = 20;
+    cout << "a = " << a << ": b = " << b << "\n";
+    cout << "a ++ = " << a++ << ": ++b = " << ++b << "\n";
+    cout << "a = " << a << ": b = " << b << "\n";
+
+    return 0;
+}
