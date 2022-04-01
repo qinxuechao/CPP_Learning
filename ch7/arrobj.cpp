140607bcb9d28c80ee856273679e681e00e29744
unknown <qinxuechao@yahoo.com>
2022-04-02 01:24:45 +0800

    Complete  ch7

---
+++ ch7/arrobj.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Xuechao Qin on 4/2/2022.
+//
+
+#include "iostream"
+#include "array"
+#include "string"
+
+// constant data
+const int Seasons = 4;
+const std::array<std::string, Seasons> Snames = {"Spring", "Summer", "Fall", "Winter"};
+
+// function to modify array object
+void fill(std::array<double, Seasons> *pa);
+
+void show(const std::array<double, Seasons>);
+
+int main() {
+    std::array<double, Seasons> expenses;
+    fill(&expenses);
+    show(expenses);
+    return 0;
+}
+
+void fill(std::array<double, Seasons> *pa) {
+    using namespace std;
+    for (int i = 0; i < Seasons; i++) {
+        cout << "Enter " << Snames[i] << " expenses: ";
+        cin >> (*pa)[i];
+    }
+}
+
+void show(std::array<double, Seasons> da) {
+    using namespace std;
+    double total = 0.0;
+    cout << "\nEXPENSES\n";
+    for (int i = 0; i < Seasons; i++) {
+        cout << Snames[i] << ":$" << da[i] << endl;
+        total += da[i];
+    }
+    cout << "Total Expenses: $" << total << endl;
+}
