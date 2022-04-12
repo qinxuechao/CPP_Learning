66b5e0b9ce05169b874d1bcfeb12470bc3a7503d
xqin <xqin@hantak.cn>
2022-04-12 17:43:59 +0800

    stash

---
+++ ch11/vector.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Xuechao Qin on 4/12/2022.
+//
+
+#include "cmath"
+#include "vector.h"
+
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::atan;
+using std::atan2;
+using std::cout;
+
+namespace VECTOR {
+    VECTOR::Vector::Vector() {
+
+    }
+
+    VECTOR::Vector::~Vector() {
+
+    }
+
+    VECTOR::Vector::Vector(double n1, double n2, VECTOR::Vector::Mode form) {
+
+    }
+
+    void VECTOR::Vector::set_x() {
+
+    }
+
+    void VECTOR::Vector::set_mag() {
+
+    }
+
+    void VECTOR::Vector::set_ang() {
+
+    }
+
+    void VECTOR::Vector::reset(double n1, double n2, VECTOR::Vector::Mode form) {
+
+    }
+
+    void VECTOR::Vector::set_y() {
+
+    }
+
+}
