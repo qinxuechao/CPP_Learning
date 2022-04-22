db294a55191e8831fb9c02df52d4377f413f2922
xqin <xqin@hantak.cn>
2022-04-22 16:19:10 +0800

    add ch16

--- ch16/str2.cpp
+++ ch16/str2.cpp
@@ -1,4 +1,26 @@
 //
-// Created by HANTAK on 4/22/2022.
+// Created by Xuechao Qin on 4/22/2022.
 //
 
+#include "iostream"
+#include "string"
+
+int main() {
+    using namespace std;
+    string empty;
+    string small = "bit";
+    string large = "Elephants are a girl's best friends";
+    cout << "Size:\n";
+    cout << "\tempty: " << empty.size() << endl;
+    cout << "\tsmall: " << small.size() << endl;
+    cout << "\tlarge:" << large.size() << endl;
+    cout << "Capacities: \n";
+    cout << "\tempty: " << empty.capacity() << endl;
+    cout << "\tsmall: " << small.capacity() << endl;
+    cout << "\tlarge: " << large.capacity() << endl;
+    empty.reserve(50);
+    cout << "Capacity after empty.reserve(50): "
+         << empty.capacity() << endl;
+
+    return 0;
+}
